e6b982dc0f89034622bf4b15fcf2d7aa8d9ca697
saranascimento9 <53081056+saranascimento9@users.noreply.github.com>
2021-09-14 20:55:51 +0100

    added the repository link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">Hello!</header>
+      <header className="App-header">
+        <h1>Hello!</h1>
+      </header>
+      <footer>
+        this website was made by{" "}
+        <a
+          href="https://www.linkedin.com/in/sara-nascimento-71a29b156/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sara
+        </a>
+        , and is{" "}
+        <a
+          href="https://github.com/saranascimento9/dictionary-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open sourced
+        </a>
+      </footer>
     </div>
   );
 }
