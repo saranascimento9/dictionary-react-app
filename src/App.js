1481d09d8462941ab99e140478b1c216691e7f85
saranascimento9 <53081056+saranascimento9@users.noreply.github.com>
2021-09-16 21:01:18 +0100

    Added the search engine

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello!</h1>
-      </header>
-      <footer>
-        this website was made by{" "}
-        <a
-          href="https://www.linkedin.com/in/sara-nascimento-71a29b156/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Sara
-        </a>
-        , and is{" "}
-        <a
-          href="https://github.com/saranascimento9/dictionary-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Hello!</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          this website was made by{" "}
+          <a
+            href="https://www.linkedin.com/in/sara-nascimento-71a29b156/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sara
+          </a>
+          , and is{" "}
+          <a
+            href="https://github.com/saranascimento9/dictionary-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
